83b4dc7ab7e761d3d8356f2e6d62833841c36e66
prashdeep <pradeep.kumar44@gmail.com>
2020-01-22 13:25:51 +0530

    committing the changes

--- springcoreexamples/src/main/java/com/priceline/core/config/ApplicationConfiguration.java
+++ springcoreexamples/src/main/java/com/priceline/core/config/ApplicationConfiguration.java
@@ -5,11 +5,13 @@ import com.priceline.core.service.AmazonPay;
 import com.priceline.core.service.PaymentGateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class ApplicationConfiguration {
 
     @Bean(value = "paymentGateway")
+    @Scope("singelon")
     public PaymentGateway paymentGateway(){
         return new AmazonPay();
     }
